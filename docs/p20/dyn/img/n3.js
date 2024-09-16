ea7d43bb31e72046f7c5cb0c7702e743305c4123
D. MacCarthy <sc8pr.py@gmail.com>
2024-09-16 17:17:56 -0600

    P20 Minor Corrctions

--- docs/p20/dyn/img/n3.js
+++ docs/p20/dyn/img/n3.js
@@ -3,7 +3,7 @@ save("p20/dyn/img/n3", {
 n3: (sel) => {
     $(sel).attr({width: 400, height: 160, "data-aspect": "5/2"});
     let svg = new SVG_Animation(sel, -4, 4, -0.2);
-    svg.image("/p20/dyn/img/walking.svg", [1.952, 3], [0.75, 1.5]);
+    svg.image("./p20/dyn/img/walking.svg", [1.952, 3], [0.75, 1.5]);
     svg.line([-4, 0], [4, 0]).attr({stroke: "black", "stroke-width": 3});
     svg.arrow([-0.2, 0.2], [-3.5, 0.2], {tail: "6"}).addClass("Blue");
     svg.arrow([0.2, 0.2], [3.5, 0.2], {tail: "6"}).addClass("Vector");
@@ -36,7 +36,7 @@ pool: (sel) => {
 rocket: (sel) => {
     $(sel).attr({width: 80, height: 340, "data-aspect": "4/17"});
     let svg = new SVG_Animation(sel, -1, 1, -3.5);
-    svg.image("/media/rocket.svg", [4, 4], [0, 0]).config({theta: 45});
+    svg.image("./media/rocket.svg", [4, 4], [0, 0]).config({theta: 45});
     svg.arrow([0, 3], [0, 5], {tail: "6"}).attr({fill: "red"});
     svg.arrow([0, -1.5], [0, -3.5], {tail: "6"}).attr({fill: "#0065FE"});
     svg.symbol("F", {vec:1}, [-0.7, 3.6]).css({fill: "red"});
