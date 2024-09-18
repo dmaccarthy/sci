baa22945c75640c7b45e79d9792578bdeaad1c49
D. MacCarthy <sc8pr.py@gmail.com>
2024-09-18 16:30:27 -0600

    S10 Compounds/Reactions Lessons

--- docs/s10/booklet.js
+++ docs/s10/booklet.js
@@ -16,11 +16,20 @@ collect.urls = {
         ["acid", "Acids & Bases"],
         ["water", "Properties of Water"],
     ],
+
+    "s10/chem3": [
+        ["rxn", "Chemical Reactions"],
+        // ["", ""],
+        // ["", ""],
+        // ["", ""],
+    ],
+
 };
 
 collect.titles = {
     "s10/chem1": "1: Matter",
     "s10/chem2": "2: Compounds",
+    "s10/chem3": "3: Reactions",
 }
 
 let course = "Science 10";
