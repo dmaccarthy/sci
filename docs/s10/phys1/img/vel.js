1287637857f67b8fd4ca75d36a60f4b023d0429c
D. MacCarthy <sc8pr.py@gmail.com>
2024-10-14 07:57:50 -0600

    S10 Velocity Lesson

---
+++ docs/s10/phys1/img/vel.js
@@ -0,0 +1,23 @@
+SVG2.cache("s10/phys1/img/vel.js", {
+
+uam: (sel) => {
+    let svg = new SVG2(sel, {size: [480, 360], lrbt: [0, 8, 0, 12], margin: [56, 12, 56, 12]});
+    svg.$.addClass("SVG2");
+    svg.grid([0, 8, 1], [0, 12, 1]);
+
+    /* Label x-axis */
+    let g = svg.group();
+    g.tick_label(0, [...range(0, 8.5, 1)], 0, "-6", "-20");
+    g.text("Time / s", [4, "-44"]);
+
+    /* Label y-axis */
+    g.tick_label(0, 0, [...range(0, 13, 2)], "-6", "-16");
+    g = g.group().config({theta: 90, shift: ["-40", 6]});
+    g.text("Position / m");
+
+    /* Plot parabola through 3 points */
+    let f = quadReg([0,0], [5,11], [8,7]).fn;
+    svg.locus(f, [0, 8]);
+},
+    
+});
