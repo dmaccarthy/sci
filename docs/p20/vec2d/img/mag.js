674b5e35dddba434a324840e90731d4125a99ba7
D. MacCarthy <sc8pr.py@gmail.com>
2024-10-15 17:41:38 -0600

    Modify P20 mag.htm

--- docs/p20/vec2d/img/mag.js
+++ docs/p20/vec2d/img/mag.js
@@ -1,40 +1,27 @@
 SVG2.cache("p20/vec2d/img/mag.js", { // Solutions for Magnitude & Direction assignment
 
 hiker: (sel) => {
-    $(sel).attr({width: 400, height: 400, "data-aspect": 1});
-    let svg = applet.vecDiagram.diagram(sel,
-        [[7, -4]],
-        {omitAxes: 1, shift: [-2, 5]}, 0.5, -3.5, -2,
-    );
-    let e = svg.items[0].element;
-    let attr = {interval: 1, fixed: 0, length: "8", omitZero: 1, offset: [0, "-18"]};
-    svg.axis({x: [-3.5, 6.5], ticks: attr}, e);
-    attr.offset = ["-14", 0];
-    svg.axis({y: [-2, 8], ticks: attr}, e);
-    svg.line([1, 5], [-2, 5], "5").css({stroke: "black", "stroke-width": 2}).after("g.Grid");
-    svg.plot([[5, 1], [-2, 5]], "5").after("g.Grid").css({"fill-opacity": 0.7, stroke: "black"});
-
-    svg.symbol("d", {vec:1, delta:1}, [2, 3.5]).css({fill: "red"});
-    svg.symbol("d", {vec:1, q4: "i"}, [-2.7, 5]).css({fill: "#0065FE"});
-    svg.symbol("d", {vec:1, q4: "f"}, [5.7, 1]).css({fill: "#0065FE"});
-    svg.text("θ", [-0.6, 4.6]).addClass("Annotate");
-    svg.text("km", [0.5, -1.5]).addClass("Annotate");
-    svg.final();
+    let svg = SVG2.vec_diag(sel, [[7, -4]], {lrbt: [-3, 6, -1, 6], scale: 40,
+        margin: 8, grid: 0.5, shift: [-2, 5], label: [1, 0, -0.3, -0.3]});
+    svg.$.find(".Component").remove();
+    svg.text("km", [5.5, 5.5]);
+    let g = svg.group();
+    g.$.addClass("Symbol F125").insertBefore(svg.$.find(".TipToTail2D"));
+    g.line([1, 5], [-2, 5]).css({stroke: "black", "stroke-width": 2});
+    g.text("θ", [-0.6, 4.56]);
+    g.plot([[5, 1], [-2, 5]], "5").$.css({"fill-opacity": 0.7});
+    g.text('Δ<tspan class="Bold">d</tspan><tspan class="F80" dx="-18" dy="-18">→</tspan>', [2, 3.5]).css({fill: "red"});
+    let d = '<tspan class="Bold">d</tspan><tspan class="F80 Ital" dy="8">#</tspan><tspan class="F80" dx="-24" dy="-26">→</tspan>';
+    g.text(d.replace("#", "i"), [-2.6, 5.2]).css({fill: "#0065fe"});
+    g.text(d.replace("#", "f"), [5.6, 1.1]).css({fill: "#0065fe"});
 },
 
 soccer: (sel) => {
-    $(sel).attr({width: 400, height: 400, "data-aspect": 1});
-    let svg = applet.vecDiagram.diagram(sel,
-        [[-10, 17.3]],
-        {omitAxes: 1}, 1, -15, -2,
-    );
-    let e = svg.items[0].element;
-    let attr = {interval: 2, fixed: 0, length: "8", omitZero: 1, offset: [0, "-18"]};
-    svg.axis({x: [-15, 5], ticks: attr}, e);
-    attr.offset = ["-14", 0];
-    svg.axis({y: [-2, 18], ticks: attr}, e);
-    svg.text("m", [0.7, 17]).addClass("Annotate");
-    svg.final();
-}
+    let svg = SVG2.vec_diag(sel, [vec2d(20, 120)], {lrbt: [-12, 2, -2, 20],
+        scale: 20, margin: 8, grid: 1, label: [2, 0, -0.5, -0.5]});
+    svg.$.find(".Component").remove();
+    svg.circle(20).css({"stroke-width": "0.5px", stroke: "grey"});
+    svg.text("m", [-11, 19]);
+},
  
 });
