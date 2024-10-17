b9386144ca8fb305e3a16d7503002b516deed7ca
D. MacCarthy <sc8pr.py@gmail.com>
2024-10-17 17:54:22 -0600

    P30 Improve helix.js animation

--- docs/p30/mag/img/helix.js
+++ docs/p30/mag/img/helix.js
@@ -5,14 +5,14 @@ ucm: (sel) => {
     svg.$.addClass("SVG2");
     let tog = [svg.circle(1)];
 
-    let [BD, SM, SM_IT] = [1, 4, 6];
+    let [BD, SM_BD, SM_IT] = [1, 5, 6];
     let arr = [0, "20"];
 
     /* Magnetic field */
     let c = {fill: "green"};
     svg.circle(0.1).css({fill: "none", stroke: c.fill});
-    svg.text("×", [0, "-3"]).css(c);
-    let sym = svg.symbol(["B", BD], ["→", SM, arr]).config({shift: [-0.25, -0.05]}).$;
+    svg.text("×", [0, "-3"]).addClass("Large").css(c);
+    let sym = svg.symbol(["B", BD], ["→", SM_BD, arr]).config({shift: [-0.25, -0.05]}).$;
     sym.addClass("Large").find("text").css(c);
 
     let q = svg.group().config({omega: 20});
@@ -21,22 +21,23 @@ ucm: (sel) => {
     let g = q.group();
     c.fill = "red";
     q.arrow({tail: [1, 0], tip: [1, 0.5]}, {tail: "6"}, "tail").$.children().css(c);
-    sym = svg.symbol(["v", BD], ["→", SM, arr]).config({shift: [0.8, 0.25]}).$;
-    sym.addClass("Large").find("text").css(c);
+    let v = q.symbol(["v", BD], ["→", SM_BD, [0, "14"]]).config({shift: [0.85, 0.25], omega: -20});
+    v.$.addClass("Large").find("text").css(c);
 
     /* Force */
     g = q.group();
     tog.push(g.$);
     c.fill = "#0065fe";
     g.arrow({tail: [1, 0], tip: [0.5, 0]}, {tail: "6"}, "tail").$.children().css(c);
-    sym = svg.symbol(["F", BD], ["→", SM, arr], ["m", SM_IT, ["14", "-8"]]).config({shift: [0.8, -0.25]}).$;
-    sym.addClass("Large").find("text").css(c);
+    let Fm = g.symbol(["F", BD], ["→", SM_BD, arr], ["m", SM_IT, ["14", "-8"]]);
+    Fm.config({shift: [0.8, -0.2], omega: -20});
+    Fm.$.addClass("Large").find("text").css(c);
 
     /* Charge */
     q.circle("5", [1, 0]).css({fill: "yellow"});
 
     // svg.$.find("text").css({ "font-size": "28px"});    
-    svg.animate(q);
+    svg.animate(q, v, Fm);
 
     clickCycle(svg.element, 3,
         () => {clickCycle.toggle(tog, true, 1)},
