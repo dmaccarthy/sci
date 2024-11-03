0df944601c3f60fc71bdaf22eea355e8d1c53aca
D. MacCarthy <sc8pr.py@gmail.com>
2024-11-03 07:06:46 -0700

    Migrate energy graphs

--- docs/p20/energy/img/cons.js
+++ docs/p20/energy/img/cons.js
@@ -25,13 +25,84 @@ Ex2: (sel) => {
 },
 
 Ex3: (sel) => {
+    let Eg = (t) => 10 * Math.pow((t - 0.05) / 0.95, 2);
     svg = SVG2.ebg(sel, 11, 1, [
-        ["E_k", (t) => t < 0.05 ? 1000 * t / 7 : 5/7 * (10 - svg.Eg(t))],
+        ["E_k", (t) => t < 0.05 ? 1000 * t / 7 : 5/7 * (10 - Eg(t))],
         ["E_elas", (t) => t < 0.05 ? 10 - 200 * t : 0],
         ["E_rotn", true],
-        ["E_g", (t) =>  t < 0.05 ? 0 : svg.Eg(t)],
+        ["E_g", (t) =>  t < 0.05 ? 0 : Eg(t)],
     ], {E: 10, margin: [4, 4, 40, 4], duration: 6});
-    svg.Eg = (t) => 10 * Math.pow((t - 0.05) / 0.95, 2);
+},
+
+Q1a: (sel) => {
+    svg = SVG2.ebg(sel, 0.5, 0.1, [
+        ["E_k", true],
+        ["E_g", (t) =>  0.14715 * t],
+        ["E_elas", (t) => t < 0.05 ? 0.45 / 0.05 * (0.05 - t) : 0],
+    ], {E: 0.45, duration: 4, margin: [40, 4, 40, 16], label: [1, "-6"]});
+},
+
+Q1b: (sel) => {
+    svg = SVG2.ebg(sel, 0.5, 0.1, [
+        ["E_k", true],
+        ["E_g", (t) =>  0.45 * t],
+        ["E_elas", (t) => t < 0.05 ? 0.45 / 0.05 * (0.05 - t) : 0],
+    ], {E: 0.45, duration: 5, margin: [40, 4, 40, 16], label: [1, "-6"]});
+},
+
+Q2: (sel) => {
+    let E = 49 * 0.225 / 2;
+    svg = SVG2.ebg(sel, 6, 1, [
+        ["E_k", (t) => E * t * t],
+        ["E_elas", true],
+    ], {E: E, duration: 0.5, label: [0, "-6"]});
+},
+
+Q3a: (sel) => {
+    let E = 7.5 * 9.81;
+    svg = SVG2.ebg(sel, 80, 5, [
+        ["E_k", (t) => E * t * t],
+        ["E_g", true],
+    ], {E: E, duration: 3, label: [0, "-6", 2]});
+},
+
+Q3b: (sel) => {
+    let Ek = 7.5 * 9.81 / 2;
+    svg = SVG2.ebg(sel, 80, 5, [
+        ["E_k", (t) => Ek * t * t],
+        ["E_g", true],
+    ], {E: 2 * Ek, duration: 1.5, label: [0, "-6", 2]});
+},
+
+Q4a: (sel) => {
+    let E = 1.2 * 9.81;
+    let Ek = 5 / 7 * E;
+    svg = SVG2.ebg(sel, 14, 1, [
+        ["E_k", (t) => Ek * t * t],
+        ["E_g", true],
+        ["E_rotn", (t) => 0.4 * Ek * t * t],
+    ], {E: E, unit: "J/kg", duration: 4, label: [0, "-6", 2]});
+},
+
+Q4b: (sel) => {
+    let E = 1.2 * 9.81;
+    let Ek = 2 / 3 * E;
+    svg = SVG2.ebg(sel, 14, 1, [
+        ["E_k", (t) => Ek * t * t],
+        ["E_g", true],
+        ["E_rotn", (t) => 0.5 * Ek * t * t],
+    ], {E: E, unit: "J/kg", duration: 4, label: [0, "-6", 2]});
+},
+
+Q5: (sel) => {
+    let E = 45 * 9.81;
+    let Ek = 6.5/15 * E;
+    svg = SVG2.ebg(sel, 500, 50, [
+        ["E_k1", (t) => 15/23.5 * Ek * t * t],
+        ["E_k2", (t) => 8.5/23.5 * Ek * t * t],
+        ["E_g1", (t) => E * (1 - t * t)],
+        ["E_g2", true],
+    ], {E: E, unit: "J", duration: 4, margin: [40, 4, 40, 16], label: [0, "-6", 2]});
 },
 
 });
---
+++ docs/p20/energy/img/mech.js
@@ -0,0 +1,54 @@
+SVG2.cache("p20/energy/img/mech.js", {
+
+Q1: (sel) => {
+    let Eg = 0.25 * 9.81 * 9.25;
+    let Ek = 72;
+    svg = SVG2.ebg(sel, 100, 10, [
+        ["E_k", true],
+        ["E_g", Eg],
+    ], {E: Eg+Ek, margin: [40, 4, 40, 16], label: [0, "-6", 2]});
+},
+
+Q2: (sel) => {
+    let Ee = 100 * 0.65 * 0.65;
+    let Eg = 0.055 * 9.81 * 1.6;;
+    svg = SVG2.ebg(sel, 50, 5, [
+        ["E_k", 0],
+        ["E_g", Eg],
+        ["E_elas", Ee],
+    ], {E: Eg+Ee, label: [0, "-6", 2]});
+},
+
+Q3: (sel) => {
+    svg = SVG2.ebg(sel, 20, 2, [
+        ["E_k", 0],
+        ["E_elas", 16],
+    ], {E: 16, unit: "mJ", label: [0, "-6", 2]});
+},
+
+Q4: (sel) => {
+    svg = SVG2.ebg(sel, 150, 25, [
+        ["E_k", true],
+        ["E_g", (t) => 125 * t * t],
+    ], {E: 125, unit: "kJ", duration: 4, margin: [40, 4, 40, 16], label: [0, "-6"]});
+},
+
+Q5: (sel) => {
+    svg = SVG2.ebg(sel, 1.5, 0.1, [
+        ["E_k", (t) => 1.35 * t * t],
+        ["E_elas", true],
+    ], {E: 1.35, duration: 0.5, margin: [40, 4, 40, 16], label: [1, "-6", 3]});
+},
+
+Q6: (sel) => {
+    let E = 1.12;
+    let Eg = (t) => E / 2 * (2 - t * t);
+    let Ek = (t) => 5/7 * (E - Eg(t));
+    svg = SVG2.ebg(sel, 1.2, 0.1, [
+        ["E_k", Ek],
+        ["E_g", Eg],
+        ["E_rotn", true],
+    ], {E: E, duration: 4, margin: [40, 4, 40, 16], label: [1, "-6", 2]});
+},
+
+});
