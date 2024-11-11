472efb309ce27ba8eb800ab9a6bc34c5cbf65f12
D. MacCarthy <sc8pr.py@gmail.com>
2024-11-11 14:55:01 -0700

    Color wheel tick marks

--- docs/cs_new/robo/img/hsv.js
+++ docs/cs_new/robo/img/hsv.js
@@ -1,23 +1,33 @@
 SVG2.cache("cs_new/robo/img/hsv.js", {
 
-wheel: (sel, h, s, v) => {
-    let svg = new SVG2(sel, {scale: 180, lrbt: [-1, 1, -1, 1], margin: [12, 12, 1, 1]});
-    let url = (v) => SVG2.cache_run("cs_new/robo/img/hsv.js", "colorWheelURL", 180, v / 100);
+wheel: (sel, h, s, v, tick) => {
+/* Draw a color wheel with tick marks and hue/sat markers using SVG2 */
+    if (tick == null) tick = 60;
     let hue, sat, val = v, r = 1.05;
-    let g = svg.group().css({stroke: "grey", "stroke-width": "2px"});
-    for (let i=0;i<3;i++) g.line(vec2d(r, 60*i), vec2d(r, 180+60*i))
-    svg.image(url(v), [2, 2]);
-    let hs = svg.group().addClass("HueSat").css({stroke: "black", "stroke-width": "2px", fill: "none"});
+    let svg = new SVG2(sel, {scale: 180, lrbt: [-1, 1, -1, 1], margin: (tick ? 10 : 1)});
 
-    svg.set_v = (v) => {
+    // Tick marks
+    if (tick) {
+        let g = svg.group().css({stroke: "grey", "stroke-width": "2px"});
+        for (let i=0;i<360;i+=tick) g.line(vec2d(r, i), vec2d(0.999, i))    
+    }
+
+    // Color wheel
+    let url = (v) => SVG2.cache_run("cs_new/robo/img/hsv.js", "colorWheelURL", 180, v / 100);
+    svg.image(url(v), [2, 2]);
+
+    svg.set_v = (v) => { // Re-draw color wheel with different Val
         if (v != val) {
             svg.$.find("image").attr({href: url(v)});
             val = v;
         }
     }
 
+    // Hue/Sat markers
+    let hs = svg.group().addClass("HueSat").css({stroke: "black", "stroke-width": "2px", fill: "none"});
+
     svg.set_hs = (h, s) => {
-        sat = hs.circle(s / 100, null, sat);
+        sat = hs.circle(s / 100, [0, 0], sat);
         hue = hs.line([0, 0], vec2d(r, h), hue);
     }
 
@@ -26,6 +36,7 @@ wheel: (sel, h, s, v) => {
 },
 
 colorWheelImg: (r, v) => {
+/* Render a color wheel as an ImageData instance */
     if (v == null) v = 1;
     let w = 2 * r + 1;
     let img = new ImageData(w, w);
@@ -46,11 +57,12 @@ colorWheelImg: (r, v) => {
         }
     return img;
 },
-    
+
 colorWheelURL: (r, v) => {
+/* Render a color wheel as a data URL */
     if (v == null) v = 1;
     let w = 2 * r + 1;
-    let cv = $("<canvas>").attr({width:w, height:w})[0];
+    let cv = $("<canvas>").attr({width: w, height: w})[0];
     let cx = cv.getContext("2d");
     let img = SVG2.cache_run("cs_new/robo/img/hsv.js", "colorWheelImg", r, v);
     cx.putImageData(img, 0, 0);
@@ -60,8 +72,9 @@ colorWheelURL: (r, v) => {
 blue: (sel) => {
     let svg = SVG2.cache_run("cs_new/robo/img/hsv.js", "wheel", sel, 0, 0, 100);
     svg.$.children("g.HueSat").remove();
-    let v = vec2d;
-    svg.path(v(0.6, 205)).arcTo(v(0.6, 260), 0.6).lineTo(v(1, 260)).arcTo(v(1, 205), 1, 2).close().update().css({fill: "none", stroke: "black", "stroke-width": "3px"});
+    let v = vec2d, r = 0.6, a = 205, b = 260;
+    let p = svg.path(v(r, a)).arcTo(v(r, b), r).lineTo(v(1, b)).arcTo(v(1, a), 1, 2).close().update();
+    p.css({fill: "none", stroke: "black", "stroke-width": "3px"});
 },
 
 });
