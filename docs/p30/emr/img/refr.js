1749783e985c8be0c48c511d370882488117d405
D. MacCarthy <sc8pr.py@gmail.com>
2024-12-05 06:03:47 -0700

    P30 Refraction tank improvement

--- docs/p30/emr/img/refr.js
+++ docs/p30/emr/img/refr.js
@@ -2,18 +2,28 @@ SVG2.cache("p30/emr/img/refr.js", {
 
 tank: (sel) => {
     svg = new SVG2(sel, {size: [640, 640], lrbt: [-1, 1], margin: 4});
-    svg.$.addClass("SVG2");
+    svg.addClass("NoStyle");
     svg.path([1, 0]).hor(-1).arcTo([1, 0], 1).close().update();
     svg.line([0, 1], [0, -1]);
+    let g = svg.group().addClass("Text");
     for (let a=1;a<90;a+=1) {
         let r = a % 10 ? (a % 5 ? 0.96 : 0.93) : 0.9;
-        if (a % 5 == 0) svg.line(vec2d(1, a), vec2d(r, a));
+        // if (a % 5 == 0)
+            svg.line(vec2d(1, a), vec2d(r, a));
         svg.line(vec2d(-1, a), vec2d(-r, a));
         if (a % 10 == 0) {
-            svg.text(90 - a, vec2d(0.85, a));
-            svg.text(90 - a, vec2d(0.85, a+180));
+            svg.delay(g.group(), {recenter: vec2d(0.85, a)}).text(90 - a);
+            svg.delay(g.group(), {recenter: vec2d(0.85, a + 180)}).text(90 - a);
         }
     }
+    let font = {"font-size": "36px"};
+    svg.delay(g.group(), {recenter: [0.45, 0.05]}).text("Boundary");
+    svg.delay(g.group(), {recenter: [0.05, 0.45]}).group().config({theta: -90}).text("Normal");
+    svg.delay(g.group(), {recenter: vec2d(0.5, -135), css: font}).text("Tank");
+    svg.delay(g.group(), {recenter: vec2d(0.5, 135), css: font}).text("Air");
+    svg.css_map("text");
+    g.css({stroke: "none", fill: "black"});
+    svg.css({fill: "none", stroke: "black"}).finalize();
 },
 
 });
