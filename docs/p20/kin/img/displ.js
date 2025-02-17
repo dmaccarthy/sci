36733fff4c7d7432cee5daa9b3ee525c47626ec8
D. MacCarthy <sc8pr.py@gmail.com>
2025-02-17 11:24:21 -0700

    P20 Kinematics Updates

--- docs/p20/kin/img/displ.js
+++ docs/p20/kin/img/displ.js
@@ -4,7 +4,7 @@ Q1a: (sel) => {
     let svg = SVG2.vec_diag(sel, [[7.2, 0]], {shift: [-4, 0],
         lrbt: [-5, 5, -1, 1], scale: 50, margin: 8, grid: 0.5, tick: "-8", label: [1, 0, 0, "-20"]});
     svg.$.find(".Component, .LabelY, .TickY").remove();
-    svg.text("km", [4.5, -0.75]);
+    svg.text("km", [4.5, -0.85]).css({"font-size": "18px"});
 },
 
 Q1b: (sel) => {
@@ -13,7 +13,7 @@ Q1b: (sel) => {
     svg.$.find(".Component, .LabelY, .TickY").remove();
     let g = svg.$.find("g.Arrow");
     $([g[0], g[2]]).remove();
-    svg.text("km", [4.5, -0.75]);
+    svg.text("km", [4.5, -0.85]).css({"font-size": "18px"});
 },
 
 Q2: (sel) => {
@@ -22,14 +22,14 @@ Q2: (sel) => {
     svg.$.find(".Component, .Resultant, .LabelY, .TickY").remove();
     let g = svg.$.find("g.Arrow");
     $([g[1], g[3]]).remove();
-    svg.text("km", [6.5, -0.75]);
+    svg.text("km", [6.5, -0.85]).css({"font-size": "18px"});
 },
 
 Q3: (sel) => {
     let svg = SVG2.vec_diag(sel, [[0, 6], [5, 0]], {lrbt: [-2, 6, -1, 7],
         scale: 50, margin: 8, grid: 0.5, tick: "-8", label: [1, 0, "-12", "-20"]});
     svg.$.find(".Component").remove();
-    svg.text("km", [5.5, -0.75]);
+    svg.text("km", [5.5, -0.85]).css({"font-size": "18px"});
 },
 
 });
