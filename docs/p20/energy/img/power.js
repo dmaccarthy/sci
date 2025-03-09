58e04963e4e950d519a51839f071adedddb0eb5f
D. MacCarthy <sc8pr.py@gmail.com>
2025-03-08 17:47:40 -0700

    P20 Power

--- docs/p20/energy/img/power.js
+++ docs/p20/energy/img/power.js
@@ -1,20 +1,17 @@
 SVG2.cache("p20/energy/img/power.js", {
 
-Q4: (sel) => {
-    let s = $(sel).attr({width: 300, height: 228, "data-aspect": "25/19"});
-    let svg = new SVG_Animation(sel, -0.27, 1.45, -0.85);
-    svg.circle(0.4, [1, 0]).css({fill: "none", stroke: "#0065FE", "stroke-width": 3});
-    let g = svg.group().css({fill: "red"});
-    let css = {"font-size": "24px"};
-    svg.text("Food", [0, 0], g).css(css);
-    svg.text("Waste", [0, -0.8], g).css(css);
-    svg.arrow([0.25, 0], 0.6, {tail: "6"}, g);
-    svg.text("1.92 kJ", [0.45, -0.1], g);
-    g = svg.group(g).config({theta: -90});
-    svg.arrow([0.1, 0], 0.6, {tail: "6"}, g);
-    svg.text("5.08 kJ", [0.35, 0.1], g);
-    svg.symbol("E", {q4: "k", scale: 1}, [0.96, 0]).css({fill: "#0065FE"});
-    svg.final();
+sprint: (sel) => {
+    let svg = new SVG2(sel, {size: [400, 330], lrbt: [-0.5, 2.9, -1.92]});
+    svg.circle(0.8, [2, 0]).css({fill: "none", stroke: "#0065FE", "stroke-width": 3});
+    svg.delay(svg.symbol(["E", 2], ["k", 6, ["16", "-6"]]), {recenter: [2, -0.07], css: {fill: "#0065fe", "font-size": "28px"}});
+    let css = {fill: "red", "font-size": "24px"};
+    svg.group("Text", {recenter: [0, -1.8], css: css}).text("Waste");
+    svg.group("Text", {recenter: [0, 0], css: css}).text("Food");
+    svg.arrow({tail: [0.5, 0], tip: [1.7, 0]}, {tail: "6"}).label("1.92 kJ", ["-20", "-24"]);
+    let a = svg.arrow({tail: [0, -0.3], tip: [1.2, -0.3]}, {tail: "6"}, "tail").config({theta: -90});
+    a.label("5.08 kJ", [0, "12"]);
+    svg.addClass("NoStyle").css_map().finalize();
+    svg.$.find("text.Small").css({"font-size": "18px"});
 },
 
 });
