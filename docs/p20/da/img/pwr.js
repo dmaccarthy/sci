ad431312c0209bb94fa8bd79fbef8770e8e2da0e
D. MacCarthy <sc8pr.py@gmail.com>
2025-04-01 17:02:41 -0600

    P20 Lessons

--- docs/p20/da/img/pwr.js
+++ docs/p20/da/img/pwr.js
@@ -7,23 +7,27 @@ lightbulb: (sel, linear) => {
     if (linear) {
         for (let i=0;i<x.length;i++) x[i] = Math.pow(x[i], -2);
         let lin = linRegXY(x, y).fn;
-        data = [{connect: [[0, lin(0)], [2, lin(2)]]}, {plot: [zip(x, y), "5"]}];
+        data = {connect: [[0, lin(0)], [2, lin(2)]]};
         lrbt = [0, 2, 0, 2];
     }
     else {
         let pwr = pwrRegXY(x, y);
         let x0 = Math.pow(2 / pwr.a, 1 / pwr.n);
-        data = [{locus: [pwr.fn, [x0, 3]]}, {plot: [zip(x, y), "5"]}];
+        data = {locus: [pwr.fn, [x0, 3]]};
         lrbt = [0, 3, 0, 2];
     }
 
+    let xLabel = "Separation / m";
+    if (linear) xLabel = `(${xLabel}) &nbsp;`;
     let svg = new SVG2(sel, {size: [480, 360], lrbt: lrbt, margin: [56, 10, 56, 12]});
     svg.graph({grid: [0.25, 2], css: true, appendAxes: 1,
-        x: {tick: [0, lrbt[1] + 0.1, 0.5], dec: 1, title: ["Separation / m", "-44"], shift: [0, "-22"]},
+        x: {tick: [0, lrbt[1] + 0.1, 0.5], dec: 1, title: [xLabel, "-44"], shift: [0, "-22"]},
         y: {tick: [0, 2.1, 0.5], dec: 1, title: ["Intensity / (W/m &nbsp; )", "-40"], shift: ["-10", "-4"]},
-        data: data,
+        data: [data, {plot: [zip(x, y), "5"]}],
     });
-    svg.delay(svg.group().config({theta: 90}), {recenter: [-0.3, 1.45]}).text("2").css({"font-size": "14px"});
+    let x0 = linear ? -0.2 : -0.3;
+    svg.delay(svg.group().config({theta: 90}), {recenter: [x0, 1.45]}).text("2").css({"font-size": "14px"});
+    if (linear) svg.delay(svg.group(), {recenter: [1.36, -0.2]}).text("–2").css({"font-size": "14px"});
 
     svg.css_map().finalize();
     svg.$.find("g.Locus").addClass("Toggle0").hide();
