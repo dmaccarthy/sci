21ff884116b6bcbf0bdc2f43288c256b21b49744
D. MacCarthy <sc8pr.py@gmail.com>
2026-03-02 18:24:33 -0700

    Updates

--- docs/media/dataimg.js
+++ docs/media/dataimg.js
@@ -4,7 +4,7 @@ function calendar_icon() {
     css(svg.rect([2, 0.4], [0, 0.8]), "#0065fe", "none@");
     css(svg.poly([[-1 , 1], [-1, -1], [1, -1], [1, 1]]), "#0065fe@8", "none");
     for (x of [1, -1]) css(svg.circle("7", [0.48 * x, 0.8]), "white");
-    svg.gtext(new Date().getDate(), ["sans", 80, "bold", "red"], [0, -0.3]);
+    svg.text(new Date().getDate(), [0, -0.3], 0, ["sans", 80, "bold", "red"]);
     return svg.element.outerHTML;
 }
 
