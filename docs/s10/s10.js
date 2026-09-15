4cc4666d5282fb0d4181b505f430e19fd9875f69
D. MacCarthy <sc8pr.py@gmail.com>
2026-09-14 21:18:35 -0600

    S10 Updates

--- docs/s10/s10.js
+++ docs/s10/s10.js
@@ -3,9 +3,9 @@ page.cal.s10 = [ // Calendar items
     ["2026.9.9", "Lab: Metals & Non-Metals", {attr: {"data-feed": "s10/chem/chem1/dot@post=handouts"}}],
     ["2026.9.15", "Quiz: Matter", {attr: {class: "Bold"}}],
     ["2026.9.17", "Lab: Ionic & Molecular Substances", {attr: {"data-feed": "s10/chem/chem2/ionic@post=handouts"}}],
-    ["12026.9.22", "Lab: Acids & Bases", {attr: {"data-feed": "s10/chem/chem2/acid@post=handouts"}}],
+    ["2026.9.22", "Lab: Acids & Bases", {attr: {"data-feed": "s10/chem/chem2/acid@post=handouts"}}],
     ["2026.9.24", "Quiz: Compounds [Tentative]", {attr: {class: "Bold"}}],
-    ["12026.9.25", "Lab: Evidence of Chemical Reactions", {attr: {"data-feed": "s10/chem/chem3/rxn@post=handouts"}}],
+    ["2026.9.25", "Lab: Evidence of Chemical Reactions", {attr: {"data-feed": "s10/chem/chem3/rxn@post=handouts"}}],
     ["12026.10.7", "Project: Antacids (Planning)", {attr: {"data-gdrv": "17KvjCnoFdUihVRdvUVlJNrnUtAyX4N0l"}}],
     ["12026.10.7", "Project: Antacids (Lab)", {attr: {"data-gdrv": "17KvjCnoFdUihVRdvUVlJNrnUtAyX4N0l"}}],
     ["12026.10.9", "Quiz: Reactions [Tentative]", {attr: {class: "Bold"}}],
@@ -43,8 +43,8 @@ _ = d => home.item("s10/chem/chem2", d);
 _({page: "ionic", title: "Binary Ionic Compounds", data: {s: "2026.9.14", a: "2026.9.16"}});
 _({page: "molec", title: "Molecular Compounds", data: {s: "2026.9.16", a: "2026.9.18"}});
 _({page: "poly", title: "Polyatomic Ions & Solubility", data: {s: "2026.9.18", a: "2026.9.21"}});
-_({page: "acid", title: "Acids & Bases", data: {s: "12026.9.21", a: "12026.9.23"}});
-_({page: "water", title: "Properties of Water", data: {s: "12026.9.23", a: "12026.9.23 16:00"}});
+_({page: "acid", title: "Acids & Bases", data: {s: "2026.9.21", a: "12026.9.23"}});
+_({page: "water", title: "Properties of Water", data: {s: "2026.9.23", a: "2026.9.23 16:00"}});
 }
 
 { // Reactions
